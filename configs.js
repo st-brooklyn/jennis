5b139ea92fae53867f802dfeb9b89afdcfa29721
Dhanakrit <dhanakrit.s@softsq.com>
2018-03-28 10:19:23 +0700

    changed to we are the world token

--- configs.js
+++ configs.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     lineconfig: {
-        channelAccessToken: 'R8eisEchDL1Js2/gK0G7m4OA8oROrGNnNZ0vwP6qa9U2G5VAFf8m2YJ/RIF1mhyCxMR26pryIXXSi52xw6KVsmBqX6eYqSesN6hnL6Ow69x2eb6CWVZgA05CHF7X2qknQAtfYqRO/dTA1CG5ItJEFQdB04t89/1O/w1cDnyilFU=',
-        channelSecret: 'c3f942444930eaacc840cee97adc7fd0'
+        channelAccessToken: 'SWqOUepjwtiRmBnGCUqQj78o5PR437Wa/z2iYEwXrk057BIjWE5DmcT/9EgtPf8a2tecqB+8SAt8Lo1vh60vbzADepL0Pv0YRpY1D6acYYJgX068kGPMOhxlexuF2r1cI2r91VwoCx4iTKv/MbtfgAdB04t89/1O/w1cDnyilFU=',
+        channelSecret: 'c88bc5de5620b44db8c26069e3cd9868'
      }
 };
