4edaf8a8245a54fd733555726ba5480149a37a1f
Dhanakrit <dhanakrit.s@softsq.com>
2018-03-28 10:08:21 +0700

    added req, res arguments

--- routes/line.js
+++ routes/line.js
@@ -8,7 +8,7 @@ const configs = require('../configs');
 const linecontroller = require('../controllers/linecontroller');
 
 /* GET home page. */
-router.post('/webhook', line.middleware(configs.lineconfig), linecontroller.webhook);
+router.post('/webhook', line.middleware(configs.lineconfig), linecontroller.webhook(req, res));
 router.get('/webhook', (req, res) => {
     console.log("hello");
 }) 
