7b74391e8e0c73f89aa69e5bbc3e5ebc3cdde940
Dhanakrit <dhanakrit.s@softsq.com>
2018-03-28 15:32:23 +0700

    added router middleware

--- routes/line.js
+++ routes/line.js
@@ -9,15 +9,28 @@ const line = require('@line/bot-sdk');
 const linecontroller = require('../controllers/linecontroller');
 
 /* GET home page. */
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+    // log each request to the console
+    console.log(JSON.stringify(req));
+
+    // continue doing what we were doing and go to the route
+    next(); 
+});
+
 router.post('/webhook', line.middleware(configs.jennisbot), (req, res) => {
     try {
         console.log(JSON.stringify(req));
+        res.status(200).end();
         //linecontroller.webhook(req, res);
     }
     catch(ex) {
         console.log(ex.stack);
     }
 });
+
+
 
 router.get('/webhook', (req, res) => {
     console.log("hello");    
